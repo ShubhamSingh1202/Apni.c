4d8cfad23450e9f8b9b7dafae1dbc5ba5eb7758d
ShubhamSingh1202 <singhshubhamsingh2007@gmail.com>
2025-07-19 18:07:04 +0530

    Update 3b.c

--- 3b.c
+++ 3b.c
@@ -1,18 +1,35 @@
 /*Write a program to check if the given number is a
  natural number.*/
  #include <stdio.h>
+ #include <string.h>
 
 int main() {
-    int num;
+    char num[100];
+    int count=0;
+    int value;
 
     printf("Enter a number: ");
-    scanf("%d", &num);
+    fgets(num,100,stdin);
+    num[strcspn(num,"\n")] = '\0';
 
-    if (num >= 1) {
-        printf("%d is a natural number.\n", num);
-    } else {
-        printf("%d is NOT a natural number.\n", num);
+    for(int i=0; num[i];i++){
+        if (num[i]=='.'||num[i=='-'])
+        {
+            count++;
+        }
+        value =value*10+num[i];
     }
+if (count==0)
+{
+  if (value >= 1) {
+        printf("%s is a natural number.\n", num);
+    } else {
+        printf("%s is NOT a natural number.\n", num);
+    }
+}else
+printf("Decimal number/Not Natural number");
+
+  
 
     return 0;
 }
